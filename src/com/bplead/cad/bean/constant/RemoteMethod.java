5d7f0d6192eceb0c7d522826f6a801c59ff37291
zjw1984 <382136791@qq.com>
2018-10-06 16:10:13 +0800

    modify by zjw 20181006 bean commit

---
+++ src/com/bplead/cad/bean/constant/RemoteMethod.java
@@ -0,0 +1,14 @@
+package com.bplead.cad.bean.constant;
+
+import priv.lee.cad.constant.RMIMethod;
+
+public class RemoteMethod extends RMIMethod {
+
+	public static final String CHECKIN = "checkin";
+
+	public static final String CHECKOUT_DOWNLOAD = "checkoutAndDownload";
+
+	public static final String GET_SIMPLE_FOLDERS = "getSimpleFolders";
+	
+	public static final String GET_SIMPLE_CONTAINERS = "getSimpleContainers";
+}
