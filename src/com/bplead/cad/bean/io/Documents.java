93464a6f1a5e70ba1f0cbb542e89fc6a6bee2403
zjw1984 <382136791@qq.com>
2018-10-27 16:13:32 +0800

    add checkRows

--- src/com/bplead/cad/bean/io/Documents.java
+++ src/com/bplead/cad/bean/io/Documents.java
@@ -7,10 +7,20 @@ public class Documents implements Serializable {
 
     private static final long serialVersionUID = -6809868832088848640L;
     
+    private List<Integer> checkRows;
+    
     private List<Document> documents;
+
+    public List<Integer> getCheckRows() {
+        return checkRows;
+    }
 
     public List<Document> getDocuments() {
         return documents;
+    }
+
+    public void setCheckRows(List<Integer> checkRows) {
+        this.checkRows = checkRows;
     }
 
     public void setDocuments(List<Document> documents) {
